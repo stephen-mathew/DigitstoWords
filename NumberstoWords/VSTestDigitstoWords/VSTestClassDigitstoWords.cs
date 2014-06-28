663e31337f3266fdb42e230e6063764ee32f4493
Stephen Mathew <evenstevensm@gmail.com>
2014-06-28 18:53:35 +0530

    added  a VS test project, and two test cases

---
+++ NumberstoWords/VSTestDigitstoWords/VSTestClassDigitstoWords.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumberstoWords;
+
+namespace VSTestDigitstoWords
+{
+    [TestClass]
+    public class VSTestClassDigitstoWords
+    {
+        [TestMethod]
+        public void TestDigitstoWords1()
+        {
+            DigitstoWords digitsWords = new DigitstoWords();
+            Assert.AreEqual("Five hundred and sixty four", digitsWords.AmountInWords("564", 1));
+        }
+        [TestMethod]
+        public void TestDigitstoWords2()
+        {
+            DigitstoWords digitsWords = new DigitstoWords();
+            Assert.AreEqual("Eight thousand five hundred and sixty four", digitsWords.AmountInWords("8564", 1));
+        }
+    }
+}
