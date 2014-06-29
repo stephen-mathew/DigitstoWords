788c2d7fcb2e77c5b6705b5d869ce5a608327d8f
Stephen Mathew <evenstevensm@gmail.com>
2014-06-29 13:44:57 +0530

    added changes to the test progam and the main program. Program starts to work.

--- NumberstoWords/NumberstoWords/DigitstoWords.cs
+++ NumberstoWords/NumberstoWords/DigitstoWords.cs
@@ -8,46 +8,51 @@ namespace NumberstoWords
 {
     public class DigitstoWords
     {
-        public string AmountInWords(string nAmount, int nSet)
+        public string AmountInWords(string nAmount, string wAmount, int nSet)
         {
             decimal val;
 
             //Checking of the input value is a number
-            if (!Decimal.TryParse(nAmount, out val))
-            {
-                return null;
-            }
+            //if (!Decimal.TryParse(nAmount, out val))
+            //{
+            //    return null;
+            //}
             string tempDecValue = "";
 
             //Checking if the input value contains a decimal point
-            if (nAmount.Contains("."))
-            {
-                tempDecValue = nAmount.Substring(nAmount.IndexOf("."));
+            //if (nAmount.Contains("."))
+            //{
+            //    tempDecValue = nAmount.Substring(nAmount.IndexOf("."));
 
-                //Using only the part before the decimal point 
-                nAmount = nAmount.Remove(nAmount.IndexOf(tempDecValue));
-            } 
+            //    //Using only the part before the decimal point 
+            //    nAmount = nAmount.Remove(nAmount.IndexOf(tempDecValue));
+            //}
 
             try
             {
                 long intAmount = Int64.Parse(nAmount);
                 if (intAmount > 0)
                 {
-                    if ((intAmount.ToString().Trim().Length / 3) > (Convert.ToInt64(nAmount.ToString().Trim().Length / 3)))
+                    //if ((intAmount.ToString().Trim().Length / 3) > (Convert.ToInt64(intAmount.ToString().Trim().Length / 3)))
+                    //{
+                    //    nSet = Convert.ToInt32(intAmount.ToString().Trim().Length / 3) + 1;
+                    //}
+                    //else
+                    //{
+                    //    nSet = Convert.ToInt32(intAmount.ToString().Trim().Length / 3);
+                    //}
+                    if (nSet == -1)
                     {
-                        nSet = Convert.ToInt32(nAmount.ToString().Trim().Length / 3) + 1;
+                        nSet = ((((float)intAmount.ToString().Trim().Length / 3) > (intAmount.ToString().Trim().Length / 3))) ? (Convert.ToInt32(intAmount.ToString().Trim().Length / 3) + 1) : Convert.ToInt32(intAmount.ToString().Trim().Length / 3);
                     }
-                    else
-                    {
-                        nSet = Convert.ToInt32(nAmount.ToString().Trim().Length / 3);
-                    }
+                    //nSet = Convert.ToInt32(intAmount.ToString().Trim().Length / 3);
                     long eAmount = Convert.ToInt64(intAmount.ToString().Trim().Substring(0, intAmount.ToString().Trim().Length - ((nSet) - 1) * 3));
-                    long multiplier = 10 ^ (((nSet - 1) * 3));
-
+                    //long multiplier = 10 ^ ((nSet - 1) * 3);
+                    long multiplier = (long)Math.Pow(10, ((nSet - 1) * 3));
                     string[] Ones = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
                     string[] Teens = { "", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
                     string[] Tens = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-                    string[] HMBT = { "", "Hundred", "Thousand", "Million", "Billion", "Trillion" };
+                    string[] HMBT = { "", "", "Thousand", "Million", "Billion", "Trillion" };
 
                     intAmount = eAmount;
 
@@ -57,7 +62,51 @@ namespace NumberstoWords
                     intAmount %= 10;
                     int nOne = (int)intAmount;
 
-
+                    //This is for hundreds
+                    if (nHundred > 0)
+                    {
+                        wAmount = wAmount + Ones[nHundred] + " Hundred ";
+                    }
+                    //This is for tens and teens
+                    if (nTen > 0)
+                    {
+                        //This is for teens
+                        if (nTen == 1 && nOne > 0)
+                        {
+                            wAmount = wAmount + Teens[nOne] + " ";
+                        }
+                        //This is for tens, 10 to 90
+                        else
+                        {
+                            wAmount = wAmount + Tens[nTen];
+                            wAmount = (nOne > 0) ? (wAmount + "-" + Ones[nOne] + " ") : wAmount + " ";
+                            //if (nOne > 0)
+                            //{
+                            //    wAmount = wAmount + "-" + Ones[nOne] + " ";
+                            //}
+                            //else
+                            //{
+                            //    wAmount = wAmount + " ";
+                            //}
+                        }
+                    }
+                    //This is for ones, 1 to 9
+                    else
+                    {
+                        if (nOne > 0)
+                        {
+                            wAmount = wAmount + Ones[nOne] + " ";
+                        }
+                    }
+                    wAmount = wAmount + HMBT[nSet] + " ";
+                    wAmount = AmountInWords(Convert.ToString(Convert.ToInt64(nAmount) - (eAmount * multiplier)).Trim() + tempDecValue, wAmount, nSet - 1);
+                }
+                else
+                {
+                    //if (Int64.Parse(nAmount) == 0)
+                    //{
+                    //    nAmount=nAmount
+                    //}
 
                 }
             }
@@ -65,7 +114,7 @@ namespace NumberstoWords
             {
 
             }
-            return "";
+            return wAmount;
         }
 
 
--- NumberstoWords/VSTestDigitstoWords/VSTestClassDigitstoWords.cs
+++ NumberstoWords/VSTestDigitstoWords/VSTestClassDigitstoWords.cs
@@ -11,13 +11,43 @@ namespace VSTestDigitstoWords
         public void TestDigitstoWords1()
         {
             DigitstoWords digitsWords = new DigitstoWords();
-            Assert.AreEqual("Five hundred and sixty four", digitsWords.AmountInWords("564", 1));
+            //long intAmount = 564;
+            //long nSet;
+            //nSet = ((intAmount.ToString().Trim().Length / 3) > (Convert.ToInt64(intAmount.ToString().Trim().Length / 3)) ?
+            //            Convert.ToInt64(intAmount.ToString().Trim().Length / 3) + 1 : Convert.ToInt64(intAmount.ToString().Trim().Length / 3));
+            Assert.AreEqual("Five Hundred and Sixty-Four", digitsWords.AmountInWords("564", null, -1));
         }
         [TestMethod]
         public void TestDigitstoWords2()
         {
             DigitstoWords digitsWords = new DigitstoWords();
-            Assert.AreEqual("Eight thousand five hundred and sixty four", digitsWords.AmountInWords("8564", 1));
+            //long intAmount = 8564;
+            //long nSet;
+            //nSet = ((intAmount.ToString().Trim().Length / 3) > (Convert.ToInt64(intAmount.ToString().Trim().Length / 3)) ?
+            //            Convert.ToInt64(intAmount.ToString().Trim().Length / 3) + 1 : Convert.ToInt64(intAmount.ToString().Trim().Length / 3));
+            Assert.AreEqual("Eight Thousand Five Hundred and Sixty-Four", digitsWords.AmountInWords("8564", null, -1));
+        }
+
+        [TestMethod]
+        public void TestDigitstoWords3()
+        {
+            DigitstoWords digitsWords = new DigitstoWords();
+            //long intAmount = 7298564;
+            //long nSet;
+            //nSet = ((intAmount.ToString().Trim().Length / 3) > (Convert.ToInt64(intAmount.ToString().Trim().Length / 3)) ?
+            //            Convert.ToInt64(intAmount.ToString().Trim().Length / 3) + 1 : Convert.ToInt64(intAmount.ToString().Trim().Length / 3));
+            Assert.AreEqual("Seven Million Two Hundred and Ninety-Eight Thousand Five Hundred and Sixty-Four", digitsWords.AmountInWords("7298564", null, -1));
+        }
+
+        [TestMethod]
+        public void TestDigitstoWords4()
+        {
+            DigitstoWords digitsWords = new DigitstoWords();
+            //long intAmount = 64;
+            //long nSet;
+            //nSet = ((intAmount.ToString().Trim().Length / 3) > (Convert.ToInt64(intAmount.ToString().Trim().Length / 3)) ?
+            //            Convert.ToInt64(intAmount.ToString().Trim().Length / 3) + 1 : Convert.ToInt64(intAmount.ToString().Trim().Length / 3));
+            Assert.AreEqual("Sixty-Four", digitsWords.AmountInWords("64", null, -1));
         }
     }
 }
